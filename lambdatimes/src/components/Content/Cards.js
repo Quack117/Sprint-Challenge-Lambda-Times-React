55ac33974283c72c2d409c2d91989857953c9c7c
Ben Q <Quackenbush117@gmail.com>
2019-06-03 20:25:22 -0600

    cards and tabs mapped

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -5,7 +5,20 @@ const Cards = props => {
   return (
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
-          new Card component for each passing the card as the only prop*/}
+          new Card component for each passing the card as the only prop*/
+          
+          props.cards.map((card) => {
+            return <div>
+                <h3>{card.headline}</h3>
+                  <p>{card.tab}</p>
+                  <img src={card.img}/>
+                  <p>{card.author}</p>
+
+              </div>
+          })
+          
+          
+          }
     </div>
   )
 }
